d181671573ab00e9c94d0bd78d2d0441c5b3329f
Gen Sasaki <sasakigen11@gmail.com>
2017-10-12 00:37:30 -0700

    Forward propagation are done

---
+++ miniflow.py
@@ -0,0 +1,98 @@
+# MiniFlow Architecture
+
+class Node(object): # same as class Node:
+    def __init__(self, inbound_nodes=[]):
+        """
+        Two lists: one to store references to the inbound nodes,
+        and the other to store references to the outbound nodes.
+        """
+        self.inbound_nodes = inbound_nodes
+        self.outbound_nodes = []
+        # For each inpund Node here, add this Node as an outbound Node to _that_ Node.
+        for n in self.inbound_nodes:
+            n.outbound_nodes.append(self)
+        self.value = None
+
+    def forward(self):
+        """
+        Forward propagation.
+
+        Compute the output value based on `inbound_nodes` and
+        store the result in self.value.
+        """
+        raise NotImplemented
+
+class Input(Node):
+    def __init__(self):
+        Node.__init__(self) # same as super.__init__(self)
+
+    def forward(self, value=None):
+        if value is not None:
+            self.value = value
+
+class Add(Node):
+    def __init__(self, x, y):
+        Node.__init__(self, [x, y]) # same as super.__init__(self, [x, y])
+
+    def forward(self):
+        x_value = self.inbound_nodes[0].value
+        y_value = self.inbound_nodes[1].value
+        self.value = x_value + y_value
+
+def topological_sort(feed_dict):
+    """
+    Sort generic nodes in topological order using Kahn's Algorithm.
+
+    `feed_dict`: A dictionary where the key is a `Input` node and the value is the respective value feed to that node.
+
+    Returns a list of sorted nodes.
+    """
+
+    input_nodes = [n for n in feed_dict.keys()]
+
+    G = {}
+    nodes = [n for n in input_nodes]
+    while len(nodes) > 0:
+        n = nodes.pop(0)
+        if n not in G:
+            G[n] = {'in': set(), 'out': set()}
+        for m in n.outbound_nodes:
+            if m not in G:
+                G[m] = {'in': set(), 'out': set()}
+            G[n]['out'].add(m)
+            G[m]['in'].add(n)
+            nodes.append(m)
+
+    L = []
+    S = set(input_nodes)
+    while len(S) > 0:
+        n = S.pop()
+
+        if isinstance(n, Input):
+            n.value = feed_dict[n]
+
+        L.append(n)
+        for m in n.outbound_nodes:
+            G[n]['out'].remove(m)
+            G[m]['in'].remove(n)
+            # if no other incoming edges add to S
+            if len(G[m]['in']) == 0:
+                S.add(m)
+    return L
+
+def forward_pass(output_nodes, sorted_nodes):
+    """
+    Performs a forward pass through a list of sorted nodes.
+
+    Arguments:
+
+        `output_node`: A node in the graph, should be the output node (have no outgoing edges).
+        `sorted_nodes`: A topologically sorted list of nodes.
+
+    Returns the output Node's value
+    """
+
+    for n in sorted_nodes:
+        n.forward()
+
+    return output_nodes.value
